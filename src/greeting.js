69c1cc66d3d8cb277f761c9956960357dc1140de
odanzhou <odanzhou@gmail.com>
2023-03-04 00:21:14 +0800

    feat: 配置及需要打包的文件

---
+++ src/greeting.js
@@ -0,0 +1,16 @@
+
+const greeting = (name) => {
+  return 'hello ' + name
+}
+
+export const hello = (name) => {
+  return 'hello, my name is ' + name
+}
+
+export default test = () => {
+  console.log('this is test')
+}
+
+export {
+  greeting
+}
